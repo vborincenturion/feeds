46dd730d9ea0d8cbe12252cfdc6dfdf06912ac5d
vborincenturion <126680910+vborincenturion@users.noreply.github.com>
2023-03-01 17:15:41 +0100

    Add files via upload

---
+++ download_db.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import os
+import urllib.request
+import zipfile
+
+# Create the "db" directory if it doesn't exist
+if not os.path.exists('db'):
+    os.makedirs('db')
+
+# URL of the file to download
+url = 'https://figshare.com/ndownloader/articles/22194535/versions/1'
+
+# Name of the file to download
+filename = '22194535.zip'
+
+# Download the file from the URL and save it to the "db" directory
+urllib.request.urlretrieve(url, os.path.join('db', filename))
+
+# Unzip the downloaded file
+with zipfile.ZipFile(os.path.join('db', filename), 'r') as zip_ref:
+    zip_ref.extractall('db')
+
