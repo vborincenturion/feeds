66b8d8f6fd2d2f05bf4e6bc2ab726f85ea5fecd6
vborincenturion <126680910+vborincenturion@users.noreply.github.com>
2023-03-14 12:33:41 +0100

    Update download_db.py

--- download_db.py
+++ download_db.py
@@ -14,8 +14,13 @@ url = 'https://figshare.com/ndownloader/articles/22194535/versions/1'
 # Name of the file to download
 filename = '22194535.zip'
 
+# Define the callback function to show the download progress
+def reporthook(count, block_size, total_size):
+    progress = count * block_size * 100 / total_size
+    print(f"Downloaded {count * block_size} bytes out of {total_size} ({progress:.1f}%)", end="\r")
+
 # Download the file from the URL and save it to the "db" directory
-urllib.request.urlretrieve(url, os.path.join('db', filename))
+urllib.request.urlretrieve(url, os.path.join('db', filename), reporthook=reporthook)
 
 # Unzip the downloaded file
 with zipfile.ZipFile(os.path.join('db', filename), 'r') as zip_ref:
